5a70a866943e11502c34408e7f9f88dfc09c2984
raihan165221 <raihan165221@gmail.com>
2021-09-27 21:15:09 +0600

    cm2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AllPerson from './componands/AllPerson/AllPerson';
+import Header from './componands/Header/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <AllPerson></AllPerson>
+    </div>
+  );
+}
+
+export default App;
